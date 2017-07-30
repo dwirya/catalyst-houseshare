602b81ded8c8abe335c1d4ae9fbb382994c3039b
TTVS <shaoroncloud@gmail.com>
2017-07-30 18:47:02 +1000

    added some components for houseDetail

--- src/components/HouseDetail.jsx
+++ src/components/HouseDetail.jsx
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import { Image, List, Table, Header, Grid, Divider, Segment } from 'semantic-ui-react';
+
+import House1 from '../media/Houses/11-Max-Avenue-St.jpg';
+
+
 
 export default class HouseDetail extends Component {
     render() {
         return (
-            <div>
-              Hello World!
-            </div>
+          <Segment.Group>
+
+            <Segment>
+              <Header as='h1'>First Header</Header>
+            </Segment>
+
+            <Image src={House1} centered />
+
+            <Segment>
+              <Header as='h2'>Second Header</Header>
+            </Segment>
+
+            <Segment>
+                <Table basic='very'>
+                  <Table.Body>
+                    <Table.Row>
+                      <Table.Cell>Rent</Table.Cell>
+                      <Table.Cell>Number of Rooms</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.Cell>Bathroom</Table.Cell>
+                      <Table.Cell>Location</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.Cell>Availability</Table.Cell>
+
+                    </Table.Row>
+                  </Table.Body>
+                </Table>
+            </Segment>
+
+            <Segment inverted>
+              <Divider inverted />
+              <Divider horizontal inverted>Horizontal</Divider>
+            </Segment>
+
+          </Segment.Group>
         )
     }
 }
