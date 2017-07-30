9548fdf6b8cef870902d047918d3a1551d3e43ec
dwirya <dwirya@outlook.com>
2017-07-30 12:24:13 +1000

    Implemented routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './index.css';
 // import App from './App';
 import Homepage from './pages/home/Homepage';
 import BrowseHouses from './pages/browse-houses/BrowseHouses';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowseHouses />, document.getElementById('root'));
+ReactDOM.render(
+    
+        <Router>
+        <div>
+            <Route exact path="/" component={ Homepage }/>
+            <Route path="/browse" component={ BrowseHouses }/>
+        </div>
+        </Router>
+        
+    , document.getElementById('root'));
 registerServiceWorker();
