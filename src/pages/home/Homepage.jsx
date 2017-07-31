d9e8763e1d825c6221ef4b369d51c75462846f9d
TTVS <shaoroncloud@gmail.com>
2017-07-31 13:11:14 +1000

    added renter welcome page

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,33 +1,17 @@
 import React, { Component } from 'react';
 
-import HousePreview from '../../containers/HousePreview.jsx';
+import HousePreview from '../../containers/HousePreview';
 import LandingSection from '../../components/LandingSection';
-import NavBar from '../../pages/home/NavigationHeader.jsx';
-
-// var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+import NavigationHeader from '../../pages/home/NavigationHeader';
 
 
 export default class Home extends React.Component {
 
-
-
-  // renderPreviews() {
-  //   var array = [1,2,3];
-  //   return array.map(() => {
-  //     return (
-  //       <div className='col-md-4'>
-  //         <HousePreview />
-  //       </div>
-  //     )
-  //   })
-  // }
-
   render() {
       return(
-        <div>
-          <NavBar />
-          <LandingSection />
-        </div>
+        <NavigationHeader />
+        <LandingSection />
       )
     }
   }
+;
---
+++ src/pages/renter/HouseInfo.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+
+import HouseDetail from '../../components/HouseDetail';
+
+
+export default class HouseInfo extends Component {
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Grid.Column>
+            <HouseDetail />
+          </Grid.Column>
+        </Grid>
+      </div>
+
+    )
+  }
+};
