0c7692db2be47afca245efc921c130170fefe0f5
TTVS <shaoroncloud@gmail.com>
2017-08-01 18:31:08 +1000

    added dropDown Menu but commented it out for later use

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { Image, List, Table, Header, Grid, Divider, Segment } from 'semantic-ui-react';
+import { Image, List, Table, Header, Grid, Dropdown, Segment } from 'semantic-ui-react';
 
 import SunsetPic from '../media/mornington-sunset.jpg';
+
+const stateOptions = [ { key: 'AL', value: 'AL', text: 'Alabama' }  ];
 
 export default class LandingSection extends Component {
     render() {
         return (
           <div>
+
+            {/* <Dropdown placeholder='State' fluid multiple search selection options={stateOptions} /> */}
+
             <Image src={SunsetPic} fluid/>
           </div>
         )
