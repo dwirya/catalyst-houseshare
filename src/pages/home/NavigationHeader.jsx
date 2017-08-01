c848342d639d39a9ca5ada62e357f5fe90ec63e2
TTVS <shaoroncloud@gmail.com>
2017-08-01 14:42:39 +1000

    changed navBar

--- src/pages/home/NavigationHeader.jsx
+++ src/pages/home/NavigationHeader.jsx
@@ -1,38 +1,56 @@
 import React, { Component } from 'react';
-import { Image, Header, Grid, Segment } from 'semantic-ui-react';
+import { Image, Header, Grid, Segment, Menu } from 'semantic-ui-react';
 
-// import House1 from '../../media/Houses/11-Max-Avenue-St.jpg';
-
-
-const Style = {
-    width: '100%',
-}
+import logo2 from '../../media/logo1.png';
 
 export default class NavigationHeader extends Component {
+  state = { activeItem: 'closest' }
 
-    render() {
-        return(
-          <div style={Style}>
-            <Grid padded columns={4}>
-                <Grid.Row>
-                  <Grid.Column>
-                    {/* <Image src={House1} size='small'/> */}
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Header as='h2'>
-                      {/* <Image src={House1} /> */}
-                      {' '}How It Works
-                    </Header>
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Header as='h2'>About Us</Header>
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Header as='h2'>Student Living</Header>
-                  </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </div>
-        )
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+      return(
+        <Menu secondary stackable widths={5}>
+          <Menu.Item>
+            <Image src = {logo2} size = 'tiny' />
+          </Menu.Item>
+
+          <Menu.Item
+            header
+            name='features'
+            // active={activeItem === 'features'}
+            onClick={this.handleItemClick}
+          >
+            Features
+          </Menu.Item>
+
+          <Menu.Item
+            header
+            name='testimonials'
+            // active={activeItem === 'testimonials'}
+            onClick={this.handleItemClick}
+          >
+            Testimonials
+          </Menu.Item>
+
+          <Menu.Item
+            header
+            name='sign-in'
+            // active={activeItem === 'sign-in'}
+            onClick={this.handleItemClick}
+          >
+            Sign-in
+          </Menu.Item>
+
+          <Menu.Item
+            header
+            name='list-a-place'
+            // active={activeItem === 'sign-in'}
+            onClick={this.handleItemClick}
+          >
+            List A Place
+          </Menu.Item>
+        </Menu>
+      )
     }
 }
