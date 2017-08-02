923510fee1d8694a9a6a41dd9ea0e237571e6a72
TTVS <shaoroncloud@gmail.com>
2017-08-02 13:51:24 +1000

    added home button and sign in button to navBar, and added activeItem state

--- src/components/NavigationHeader.jsx
+++ src/components/NavigationHeader.jsx
@@ -9,46 +9,49 @@ export default class NavigationHeader extends Component {
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
+
+    const { activeItem } = this.state
+    
       return(
-        <Menu secondary stackable widths={4}>
+        <Menu secondary stackable widths={5}>
           <Menu.Item>
             <Image src = {logo2} size = 'tiny' />
           </Menu.Item>
 
           <Menu.Item
             header
-            // name='testimonials'
-            // active={activeItem === 'testimonials'}
-            onClick={this.handleItemClick}
+            // name='home'
+            // active={activeItem === 'home'}
+            // onClick={this.handleItemClick}
           >
-            Home
+            {/* Home */}
           </Menu.Item>
 
           <Menu.Item
             header
             name='find-a-place'
-            // active={activeItem === 'find-a-place'}
+            active={activeItem === 'find-a-place'}
             onClick={this.handleItemClick}
           >
             Find A Place
           </Menu.Item>
 
-          {/* <Menu.Item
-            header
-            name='sign-in'
-            // active={activeItem === 'sign-in'}
-            onClick={this.handleItemClick}
-          >
-            Sign-in
-          </Menu.Item> */}
-
           <Menu.Item
             header
             name='list-a-place'
-            // active={activeItem === 'sign-in'}
+            active={activeItem === 'list-a-place'}
             onClick={this.handleItemClick}
           >
             List A Place
+          </Menu.Item>
+
+          <Menu.Item
+            header
+            name='sign-in'
+            active={activeItem === 'sign-in'}
+            onClick={this.handleItemClick}
+          >
+            Sign-in
           </Menu.Item>
         </Menu>
       )
